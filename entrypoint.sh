c0b37b3d95f43f00501b59fe430527570681eb7b
Contention <barry@contention.agency>
2023-03-24 19:40:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 
 # Do deployment
-sh -c "rsync $FLAGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $EXCLUDES $GITHUB_WORKSPACE/$LOCALPATH $USER@$HOST:$REMOTEPATH"
+sh -c "rsync $INPUT_FLAGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $INPUT_EXCLUDES $GITHUB_WORKSPACE/$INPUT_LOCALPATH $INPUT_USER@$INPUT_HOST:$INPUT_REMOTEPATH"
