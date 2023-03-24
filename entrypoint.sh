a51ddb22fb530ceabcd5c6b31ef805db85dac65c
Contention <barry@contention.agency>
2023-03-24 21:09:24 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 # Set deploy key
 SSH_PATH="$HOME/.ssh"
 mkdir "$SSH_PATH"
-echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
+echo "$INPUT_DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 
